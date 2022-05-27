2f3dde0c81efb772f484fe099bd0d6048dce4eef
mxzoom <mxzoom@mail.ru>
2022-05-27 11:27:32 +0300

    add go-yaml support

--- lesson8/config/config.go
+++ lesson8/config/config.go
@@ -9,14 +9,15 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 type MyConf struct {
-	port        string
-	db_url      string
-	some_app_id string
-	some_url    string
+	Port        string `yaml:"port"`
+	Db_url      string `yaml:"db_url"`
+	Some_app_id string `yaml:"some_app_id"`
+	Some_url    string `yaml:"some_url"`
 	checked     bool
 	err         []error
 	source      string
@@ -26,10 +27,10 @@ type MyConf struct {
 func (cfg *MyConf) FlagParse() {
 	args := flag.NewFlagSet("args", flag.ExitOnError)
 	file := flag.NewFlagSet("file", flag.ExitOnError)
-	args.StringVar(&cfg.port, "port", "", "type port number")
-	args.StringVar(&cfg.db_url, "db_url", "", "type db_url")
-	args.StringVar(&cfg.some_app_id, "some_app_id", "", "type some_app_id")
-	args.StringVar(&cfg.some_url, "some_url", "", "type some url")
+	args.StringVar(&cfg.Port, "port", "", "type port number")
+	args.StringVar(&cfg.Db_url, "db_url", "", "type db_url")
+	args.StringVar(&cfg.Some_app_id, "some_app_id", "", "type some_app_id")
+	args.StringVar(&cfg.Some_url, "some_url", "", "type some url")
 	file.StringVar(&cfg.path, "path", "", "type path to config file")
 	if len(os.Args) >= 2 {
 		switch os.Args[1] {
@@ -54,38 +55,30 @@ func (cfg *MyConf) FlagParse() {
 }
 
 func (cfg *MyConf) LoadCfgFromFile() {
-	cfgMap := make(map[string]string)
 	file, err := ioutil.ReadFile(cfg.path)
 	if err != nil {
 		fmt.Println("Укажите верный путь для файла конфигурации")
 		os.Exit(2)
 	}
-	lines := strings.Split(string(file), "\n")
-	for i := range lines {
-		temp := strings.SplitN(lines[i], ":", 2)
-		if len(temp) == 2 {
-			cfgMap[temp[0]] = strings.ReplaceAll((temp[1]), " ", "")
-		}
+	yamlErr := yaml.Unmarshal(file, cfg)
+	if yamlErr != nil {
+		fmt.Println("Файл конфигурации отсутсвует, или имеет неизвестный формат")
+		os.Exit(2)
 	}
-	cfg.port = cfgMap["port"]
-	cfg.db_url = cfgMap["db_url"]
-	cfg.some_url = cfgMap["some_url"]
-	cfg.some_app_id = cfgMap["some_app_id"]
-	cfg.source = "file"
 
 }
 
 func (cfg *MyConf) ParseEnv() {
-	cfg.port = os.Getenv("port")
-	cfg.db_url = os.Getenv("db_url")
-	cfg.some_app_id = os.Getenv("some_app_id")
-	cfg.some_url = os.Getenv("some_url")
+	cfg.Port = os.Getenv("port")
+	cfg.Db_url = os.Getenv("db_url")
+	cfg.Some_app_id = os.Getenv("some_app_id")
+	cfg.Some_url = os.Getenv("some_url")
 	cfg.source = "env"
 }
 
 func (cfg *MyConf) PrintCfg() {
 	if cfg.checked {
-		fmt.Printf("%s\n%s\n%s\n%s\n", cfg.port, cfg.db_url, cfg.some_app_id, cfg.some_url)
+		fmt.Printf("%s\n%s\n%s\n%s\n", cfg.Port, cfg.Db_url, cfg.Some_app_id, cfg.Some_url)
 		switch cfg.source {
 		case "env":
 			{
@@ -109,14 +102,14 @@ func (cfg *MyConf) PrintCfg() {
 }
 
 func (cfg *MyConf) CheckArgs() {
-	if cfg.some_app_id == "" {
+	if cfg.Some_app_id == "" {
 		cfg.err = append(cfg.err, errors.New("не указан идентификатор приложения [a-Z, 0-9]"))
 	}
-	_, portErr := strconv.Atoi(cfg.port)
+	_, portErr := strconv.Atoi(cfg.Port)
 	if portErr != nil {
 		cfg.err = append(cfg.err, errors.New("не указан номер порта [0-9]{1-6}"))
 	}
-	_, ok := url.ParseRequestURI(cfg.some_url)
+	_, ok := url.ParseRequestURI(cfg.Some_url)
 	if ok != nil {
 		cfg.err = append(cfg.err, errors.New("не указан валидный url"))
 	}
@@ -124,7 +117,7 @@ func (cfg *MyConf) CheckArgs() {
 		cfg.checked = true
 	}
 	re, _ := regexp.Compile(`^postgres://.+:.+@.+:\d{0,5}/.+\?sslmode=(enable$|disable$)`)
-	if !re.MatchString(cfg.db_url) {
+	if !re.MatchString(cfg.Db_url) {
 		cfg.err = append(cfg.err, errors.New("не указана валидная строка подключения к БД, например [postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable]"))
 	}
 	if len(cfg.err) == 0 {
